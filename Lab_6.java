9e2d66a38626b0d4ba95eb83278897d20615af74
Dan-live <109356212+Dan-live@users.noreply.github.com>
2023-05-20 01:13:55 +0300

    changes after consultation

--- Lab_6.java
+++ Lab_6.java
@@ -1,3 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.Buffer;
+import java.nio.file.FileSystems;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.BrokenBarrierException;
@@ -10,8 +16,14 @@ import java.util.Random;
 class Elem {
     int data;
 }
-
-
+class File
+{
+    public static FileWriter file;
+}
+class Barrier 
+{
+    public static  CyclicBarrier br = new CyclicBarrier(2);
+}
 class SharedSemaphore
 {
 	/* Ініціалізація семафорів */
@@ -27,17 +39,23 @@ class CR2{
 }
 class VectorBuffer {
     private static final int SIZE = 15;
-    private Elem[] vec = new Elem[SIZE];
+   // private Elem[] vec = new Elem[SIZE];
+    private int[] buff;
     private int ind1, ind2;
     public static int full_break = 0;
     public static int empty_break = 0;
+    public static int elementsQuantity = 0;
     public static int num_of_operations = 70;
-    public static VectorBuffer buff = new VectorBuffer();
+    //public static int buff = new VectorBuffer();
 
     VectorBuffer() {
-        for (int i = 0; i < SIZE; i++) {
-            vec[i] = new Elem();
-        }
+        // for (int i = 0; i < SIZE; i++) {
+        //     vec[i] = new Elem();
+        // }
+        ind1 = 0;
+        ind2 = 0;
+        elementsQuantity = 0;
+        buff = new int[SIZE];
     }
 
     boolean isFull() {
@@ -48,38 +66,67 @@ class VectorBuffer {
         return ind2 <= 0;
     }
 
-    synchronized int Set(int data) {
-        if (ind2 != 0) {
-            ind1 = (ind1 + 1) % SIZE;
+    synchronized void Set(int data, String str) {
+        while(elementsQuantity == buff.length){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Producer error: " + e);
+            }
         }
-        vec[ind1].data = data;
-        ind2++;
-        return data;
+
+        buff[ind1] = data;
+        
+
+        System.out.println("Producer " + str +" in write to buffer[" + ind1 + "] value: " + data + "\n");
+
+
+
+        ind1 = (ind1 + 1) % buff.length;
+        elementsQuantity++;
+        if(ind1 == 0) full_break++;
+        notify();
     }
 
-    synchronized int Get() {
-        int result = vec[(ind1 - ind2 + 1 + SIZE) % SIZE].data;
-        ind2--;
-        return result;
+    synchronized void Get(String str) {
+
+        while(elementsQuantity == 0){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("Consumer error: " + e);
+            }
+        }
+        System.out.println("Consumer " + str +" get in buffer[" + ind2 + "] value: " + buff[ind2] + "\n");
+        
+        //int result = vec[(ind1 - ind2 + 1 + SIZE) % SIZE].data;
+        ind2 = (ind2 + 1) % SIZE;
+        elementsQuantity--; 
+        if(ind2 == 0) empty_break++;
+
+        notify();
+        //return vec[ind2];
     }
 
-     boolean isExit() {
-        return full_break >= 2 && empty_break >= 2;
+    public static boolean isExit() {
+        return (full_break >= 2 && empty_break >= 2);
     }
 }
 
 class P1 extends Thread
 {
     int num ;
+    private final VectorBuffer vect;
     // private P2 thread2;
     // private P3 thread3;
     // private P4 thread4;
     // private P5 thread5;
     // private P6 thread6;
 
-    P1(/*P2 thread2, P3 thread3, P4 thread4, P5 thread5, P6 thread6*/) 
+    P1(VectorBuffer CR1) 
     {
         super("P1");
+        this.vect = CR1;
         // this.thread2 = thread2;
         // this.thread3 = thread3;
         // this.thread4 = thread4;
@@ -90,22 +137,30 @@ class P1 extends Thread
     public void run() 
     {
         System.out.println("Thread 1 is started\n");
-        while(true)
+        while(!VectorBuffer.isExit())
         {
-            if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
-            if(!VectorBuffer.buff.isEmpty())
-            {
-                System.out.println("Thread 1 get value " + VectorBuffer.buff.Get() + " from CR1\n");
-                if(VectorBuffer.buff.isExit()) VectorBuffer.empty_break++;
-                VectorBuffer.num_of_operations--;
-            }
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            //if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
+            
+                //System.out.println("Thread 1 get value \n");
+                vect.Get(getName());
+                //VectorBuffer.buff.Get();
+            
+            // try {
+            //     Thread.sleep(10);
+            // } catch (InterruptedException e) {
+            //     e.printStackTrace();
+            // }
+           
+            
 
         }
+
+        if (!Barrier.br.isBroken()){
+            Barrier.br.reset();
+        }
+        SharedSemaphore.thread_sem1.release();
+        SharedSemaphore.thread_sem1.release();
+        System.out.println("P1 finished!\n");
 
         // thread2.interrupt();
         // System.out.println("p1| p2 canceled\n");
@@ -123,17 +178,21 @@ class P1 extends Thread
 
 class P2 extends Thread
 {
-    P2() 
+    private final VectorBuffer vect;
+    P2(VectorBuffer CR1) 
     {
         super("P2");
+        this.vect = CR1;
         start();
     }
     public void run() 
     {
         System.out.println("Thread 2 is started\n");
-        while(true)
+        while(!VectorBuffer.isExit())
         {
-            if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
+            
+                
+            //if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
             //повна синхронізація Sem1 + Sem2
 
             System.out.println("\nThread_2 opens semaphore thread_sem2 for the Thread_3");
@@ -157,11 +216,22 @@ class P2 extends Thread
 
             //поклав 
             Random rand = new Random();
-            System.out.println("Thread 2 set value " + VectorBuffer.buff.Set(rand.nextInt(1000)) + " to CR1\n");
-            if(VectorBuffer.buff.isExit()) VectorBuffer.full_break++;
-            VectorBuffer.num_of_operations--;
+            //System.out.println("Thread 2 set value \n");
+            vect.Set(rand.nextInt(1000), getName());
+            // System.out.println("Thread 2 set value " + VectorBuffer.buff.Set(rand.nextInt(1000)) + " to CR1\n");
+            // if(VectorBuffer.buff.isExit()) VectorBuffer.full_break++;
+            // VectorBuffer.num_of_operations--;
+            
+            
+            
         }
-
+        
+        if (!Barrier.br.isBroken()){
+            Barrier.br.reset();
+        }
+        SharedSemaphore.thread_sem1.release();
+        SharedSemaphore.thread_sem1.release();
+        System.out.println("P2 finished!\n");
     }
 
 
@@ -180,9 +250,9 @@ class P3 extends Thread
     public void run() 
     {
         System.out.println("Thread 3 is started");
-        while(true)
+        while(!VectorBuffer.isExit())
         {
-            if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
+            //if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
 
             //модивікація змінних
             
@@ -196,7 +266,7 @@ class P3 extends Thread
 
             //циклічний бар'єр 
 
-            System.out.println("\nThread_2  waits until Thread_1 reaches synchronization point");
+            System.out.println("\nThread_3  waits until Thread_6 reaches synchronization point");
             try
             {
               /* Потік 2 чекає, доки потік 1 дійде до точки синхронізації */  
@@ -240,68 +310,106 @@ class P3 extends Thread
  		  	}
 
  		     System.out.println("\nThread_3 works AFTER semaphore thread_sem2");
-              VectorBuffer.num_of_operations--;
-
+              //VectorBuffer.num_of_operations--;
+              
         }
-
+            
+            
+          if (!Barrier.br.isBroken()){
+            Barrier.br.reset();
+          }
+          SharedSemaphore.thread_sem1.release();
+          SharedSemaphore.thread_sem1.release();
+          System.out.println("P3 finished!\n");
     }
+
 }
+
 
 class P4 extends Thread
 {
 
-    private CyclicBarrier br;
-    int num ;
-    P4() 
+    //int num ;
+    private final VectorBuffer vect;
+    P4(VectorBuffer CR1) 
     {
         super("P4");
+        this.vect = CR1;
         start();
     }
     public void run() 
     {
         System.out.println("Thread 4 is started\n");
-        while(true)
+        while(!VectorBuffer.isExit())
         {
-            if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
-            if(!VectorBuffer.buff.isEmpty())
-            {
-                System.out.println("Thread 4 get value " +  VectorBuffer.buff.Get() + " from CR1\n");
-                if(VectorBuffer.buff.isExit()) VectorBuffer.empty_break++;
-                VectorBuffer.num_of_operations--;
-            }
-
+            //if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
+            
+                //System.out.println("Thread 4 get value \n");
+                vect.Get(getName());
+                //VectorBuffer.buff.Get();
+            
+            // try {
+            //     Thread.sleep(10);
+            // } catch (InterruptedException e) {
+            //     e.printStackTrace();
+            // }
+            
+            
         }
-
+        
+        if (!Barrier.br.isBroken()){
+            Barrier.br.reset();
+        }
+        SharedSemaphore.thread_sem1.release();
+        SharedSemaphore.thread_sem1.release();
+        System.out.println("P4 finished!\n");
 
     }
 }
 
 class P5 extends Thread
 {
-    P5() 
+    private final VectorBuffer vect;
+    P5(VectorBuffer CR1) 
     {
         super("P5");
+        this.vect = CR1;
         start();
     }
     public void run() 
     {
         System.out.println("Thread 5 is started\n");
-        while(true)
+        while(!VectorBuffer.isExit())
         {
-            if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
-            Random rand = new Random();
-            System.out.println("Thread 5 set value " + VectorBuffer.buff.Set(rand.nextInt(1000)) + " to CR1\n");
-            if(VectorBuffer.buff.isExit()) VectorBuffer.full_break++;
-            VectorBuffer.num_of_operations--;
+            //if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
+            
+                Random rand = new Random();
+               // System.out.println("Thread 5 set value \n");
+                vect.Set(rand.nextInt(1000), getName());
+                //VectorBuffer.buff.Get();
+            
+            // try {
+            //     Thread.sleep(10);
+            // } catch (InterruptedException e) {
+            //     e.printStackTrace();
+            // }
+            
+            
         }
-
+        
+        if (!Barrier.br.isBroken()){
+            Barrier.br.reset();
+        }
+        SharedSemaphore.thread_sem1.release();
+        SharedSemaphore.thread_sem1.release();
+        System.out.println("P5 finished!\n");
     }
 }
 
 class P6 extends Thread
 {
 
-    
+
     private CyclicBarrier br;
     P6(CyclicBarrier brInit) 
     {
@@ -312,12 +420,12 @@ class P6 extends Thread
     public void run() 
     {
         System.out.println("Thread 6 is started\n");
-        while(true)
+        while(!VectorBuffer.isExit())
         {
-            if (VectorBuffer.buff.isExit() || VectorBuffer.num_of_operations <= 0) break;
+            //if (VectorBuffer.isExit() || VectorBuffer.num_of_operations <= 0) break;
             
             //циклічний бар'єр
-            System.out.println("\nThread_2  waits until Thread_1 reaches synchronization point");
+            System.out.println("\nThread_6  waits until Thread_3 reaches synchronization point");
             try
             {
               /* Потік 2 чекає, доки потік 1 дійде до точки синхронізації */  
@@ -340,47 +448,64 @@ class P6 extends Thread
             System.out.println("num_of_sum = " + num_of_sum);
             System.out.println("Thread 6 unlocked mutex");
 			CR2.mutex.unlock();
-            VectorBuffer.num_of_operations--;
+            //VectorBuffer.num_of_operations--;
+
         }
         
-
+        
+        if (!Barrier.br.isBroken()){
+            Barrier.br.reset();
+        }
+        SharedSemaphore.thread_sem1.release();
+        SharedSemaphore.thread_sem1.release();
+        System.out.println("P6 finished!\n");
     }
 }
 
 class Main {
-    private static  CyclicBarrier br = new CyclicBarrier(2);
+    
 
     public static void main(String[] args) {
-        int first_length = 4;
-        Random rand = new Random();
 
-        for (int i = 0; i < first_length; i++) {
-            System.out.println(VectorBuffer.buff.Set(rand.nextInt(1000)));
-        }
+        //int first_length = 4;
+        //Random rand = new Random();
 
-        System.out.println("Array filled by elements from 0-th to " + (first_length - 1)+ "\n");
+        // for (int i = 0; i < first_length; i++) {
+        //     System.out.println(VectorBuffer.buff.Set(rand.nextInt(1000)));
+        // }
 
-        P1 thread1 = new P1(/*thread2, thread3, thread4, thread5, thread6*/);
-        P2 thread2 = new P2();
-        P3 thread3 = new P3(br);
-        P4 thread4 = new P4();
-        P5 thread5 = new P5();
-        P6 thread6 = new P6(br);
+        //System.out.println("Array filled by elements from 0-th to " + (first_length - 1)+ "\n");
+
+
         
      
 
         try
         {
+            FileWriter file = new FileWriter("log/result.log");
+            VectorBuffer CR1 = new VectorBuffer();
+            P1 thread1 = new P1(CR1);
+            P2 thread2 = new P2(CR1);
+            P3 thread3 = new P3(Barrier.br);
+            P4 thread4 = new P4(CR1);
+            P5 thread5 = new P5(CR1);
+            P6 thread6 = new P6(Barrier.br);
+
             thread1.join();
             thread2.join();
             thread3.join();
             thread4.join();
             thread5.join();
             thread6.join();
+            
+            System.out.println("Main finished");
+            file.write("Main finished");
+            file.close();
         }
-        catch(InterruptedException e)
+        catch(IOException | InterruptedException e)
         {
             System.out.println("Main thred intrerapted");
         }
+
     }
 }
